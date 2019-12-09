79c2a1b97f8a3a21c22b6f230c354002d565fc21
Brandon Chung <bchung014@gmail.com>
2019-12-09 17:03:54 -0500

    added rails_12factor gem

--- frontend/discord.jsx
+++ frontend/discord.jsx
@@ -8,6 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
 
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
 
 
   ReactDOM.render(<h1>dischord</h1>, root);

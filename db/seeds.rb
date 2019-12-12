8e43b4bb857731ee2b2291d073017104d7530da5
Brandon Chung <bchung014@gmail.com>
2019-12-12 09:47:19 -0500

    Added splash.scss and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+ServerMembership.destroy_all
+Server.destroy_all
+User.destroy_all
+
 User.create!(username: 'warreng', email: 'warreng@gmail.com', password: 'hunter2')
 User.create!(username: 'natedogg', email: 'natedogg@regulate.com', password: 'hunter2')
 User.create!(username: 'fatlip', email: 'fatlip@regulate.com', password: 'hunter2')
 User.create!(username: 'macdre', email: 'macdre@regulate.com', password: 'hunter2')
 
-Server.create!(name: 'oakland', admin_id: 1)
-Server.create!(name: 'thebay', admin_id: 1)
-Server.create!(name: 'sanjose', admin_id: 4)
+# Server.create!(name: 'oakland', admin_id: 1)
+# Server.create!(name: 'thebay', admin_id: 1)
+# Server.create!(name: 'sanjose', admin_id: 4)
 
-ServerMembership.create!(server_id: 1, user_id: 1)
-ServerMembership.create!(server_id: 2, user_id: 1)
-ServerMembership.create!(server_id: 3, user_id: 4)
+# ServerMembership.create!(server_id: 1, user_id: 1)
+# ServerMembership.create!(server_id: 2, user_id: 1)
+# ServerMembership.create!(server_id: 3, user_id: 4)
 
-ServerMembership.create!(server_id: 1, user_id: 2)
-ServerMembership.create!(server_id: 1, user_id: 3)
-ServerMembership.create!(server_id: 1, user_id: 4)
+# ServerMembership.create!(server_id: 1, user_id: 2)
+# ServerMembership.create!(server_id: 1, user_id: 3)
+# ServerMembership.create!(server_id: 1, user_id: 4)
